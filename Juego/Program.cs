92108fb6df1592895ec9ef1214a6a0f1ffe44c99
Patricio Agustín Jimenez Paz <patojimenezpaz@gmail.com>
2022-07-06 16:50:53 -0300

    Añadida funcinalidad de guardado y carga de personajes mediante json

--- Juego/Program.cs
+++ Juego/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.Clear();
+﻿using System.Text.Json;
+
+Console.Clear();
 Console.WriteLine("======================================");
 Console.WriteLine("====== Bienvenido a Combates Mu ======");
 Console.WriteLine("======================================");
@@ -30,20 +32,19 @@ do {
             break;
         case ConsoleKey.D2:
             Console.Clear();
-            Console.Write("\nIngrese la cantidad de personajes que habrá: ");
-            string entrada = Console.ReadLine();
-            int cantPsjes = 0;
-            while (!Int32.TryParse(entrada, out cantPsjes) && cantPsjes < 2) {
-                Console.Write("\nError! Debe ingresar un número válido: ");
-                entrada = Console.ReadLine();
-            }
-            cargarPersonajes(personajes, cantPsjes);
+            cargarPersonajes(personajes);
             cantPersonajes = personajes.Count;
             break;
         case ConsoleKey.D3:
             Console.Clear();
+            cargarPersonajesDesdeJson(personajes);
+            cantPersonajes = personajes.Count;
             break;
         case ConsoleKey.D4:
+            Console.Clear();
+            guardarPersonajesJson(personajes);
+            break;
+        case ConsoleKey.D5:
             Console.Clear();
             IniciarCombates(personajes, ref cantPersonajes, ref combates, ref cantCombates);
             break;
@@ -61,15 +62,32 @@ Console.WriteLine("\nPresione cualquier tecla para continuar...");
 Console.ReadKey();
 // End
 
-static void cargarPersonajes(List<Personaje> listaPersonajes, int cantPersonajes) {
-    for (int i = 1; i <= cantPersonajes; i++) {
+static void cargarPersonajes(List<Personaje> listaPersonajes) {
+    Console.Write("\nIngrese la cantidad de personajes que habrá: ");
+    string entrada = Console.ReadLine();
+    int cantPsjes = 0;
+    while (!Int32.TryParse(entrada, out cantPsjes) && cantPsjes < 2) {
+        Console.Write("\nError! Debe ingresar un número válido: ");
+        entrada = Console.ReadLine();
+    }
+    for (int i = 1; i <= cantPsjes; i++) {
         listaPersonajes.Add(new Personaje());
     }
+    using (StreamWriter sw = new StreamWriter("jugadores.json")) {
+        var jsonOptions = new JsonSerializerOptions { WriteIndented = true };
+        sw.WriteLine(JsonSerializer.Serialize(listaPersonajes, jsonOptions));
+        sw.Close();
+    }
+    Console.WriteLine("Generando personajes...");
+    Thread.Sleep(1500);
+    Console.WriteLine("Personajes generados con éxito.");
+    Console.WriteLine("\nPresione cualquier tecla para continuar...");
+    Console.ReadKey();
 }
 
 static void mostrarPersonajes(List<Personaje> personajes) {
     if (personajes.Count > 0) {
-        Console.WriteLine("\n========================");
+        Console.WriteLine("========================");
         Console.WriteLine("====== Personajes ======");
         Console.WriteLine("========================");
         for (int i = 0; i < personajes.Count; i++) {
@@ -131,6 +149,8 @@ static void IniciarCombates(List<Personaje> personajes, ref int cantPersonajes, 
     Console.WriteLine("====== Ganador ======");
     Console.WriteLine("=====================");
     Console.WriteLine(personajes[0].ToString());
+    Console.WriteLine("\nPresione cualquier tecla para continuar...");
+    Console.ReadKey();
 }
 
 static void MostrarRanking() {
@@ -141,7 +161,7 @@ static void MostrarRanking() {
                                        .ThenBy(x => x.Split[0] + x.Split[1])
                                        .Select(x => x.Str)
                                        .ToList();
-        Console.WriteLine("\n=====================");
+        Console.WriteLine("=====================");
         Console.WriteLine("====== Ranking ======");
         Console.WriteLine("=====================");
         foreach (string psje in sortedGanadores) {
@@ -155,5 +175,36 @@ static void MostrarRanking() {
     }
 
     Console.Write("\nPresione cualquier tecla para continuar...");
+    Console.ReadKey();
+}
+
+static void cargarPersonajesDesdeJson(List<Personaje> personajes) {
+    Console.WriteLine("Cargando personajes desde \"jugadores.json\"...");
+    using (StreamReader sr = new StreamReader("jugadores.json")) {
+        List<Personaje> psjesCargados = JsonSerializer.Deserialize<List<Personaje>>(sr.ReadToEnd());
+        foreach (Personaje psje in psjesCargados) {
+            personajes.Add(psje);
+        }
+        Thread.Sleep(1000);
+        if (psjesCargados.Count > 0) {
+            Console.WriteLine($"Cargados {psjesCargados.Count} personajes.");
+        } else {
+            Console.WriteLine("No hay personajes para cargar.");
+        }
+        sr.Close();
+    }
+    Console.WriteLine("\nPresione cualquier tecla para continuar...");
+    Console.ReadKey();
+}
+
+static void guardarPersonajesJson(List<Personaje> personajes) {
+    Console.WriteLine("Guardando personajes en \"jugadores.json\"...");
+    using (StreamWriter sw = new StreamWriter("jugadores.json")) {
+        var jsonOptions = new JsonSerializerOptions { WriteIndented = true };
+        sw.WriteLine(JsonSerializer.Serialize(personajes, jsonOptions));
+    }
+    Thread.Sleep(1000);
+    Console.WriteLine("Guardado exitoso.");
+    Console.WriteLine("\nPresione cualquier tecla para continuar...");
     Console.ReadKey();
 }
